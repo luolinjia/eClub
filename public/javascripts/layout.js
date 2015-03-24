5844767dc30611d13d7db78865c48ab57d63a0f3
luolinjia <360512239@qq.com>
2015-03-25 02:10:32 +0800

    Update the footer and dynamic page

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,6 +1,7 @@
 /**
  * Created by luolinj on 3/9/2015.
  */
+(function($,h,c){var a=$([]),e=$.resize=$.extend($.resize,{}),i,k="setTimeout",j="resize",d=j+"-special-event",b="delay",f="throttleWindow";e[b]=250;e[f]=true;$.event.special[j]={setup:function(){if(!e[f]&&this[k]){return false}var l=$(this);a=a.add(l);$.data(this,d,{w:l.width(),h:l.height()});if(a.length===1){g()}},teardown:function(){if(!e[f]&&this[k]){return false}var l=$(this);a=a.not(l);l.removeData(d);if(!a.length){clearTimeout(i)}},add:function(l){if(!e[f]&&this[k]){return false}var n;function m(s,o,p){var q=$(this),r=$.data(this,d);r.w=o!==c?o:q.width();r.h=p!==c?p:q.height();n.apply(this,arguments)}if($.isFunction(l)){n=l;return m}else{n=l.handler;l.handler=m}}};function g(){i=h[k](function(){a.each(function(){var n=$(this),m=n.width(),l=n.height(),o=$.data(this,d);if(m!==o.w||l!==o.h){n.trigger(j,[o.w=m,o.h=l])}});g()},e[b])}})(jQuery,this);
 $(function() {
     var userIcon = $('.icon-user').parent();
     _layout.handleSession();
@@ -203,7 +204,18 @@ var _layout = {
     },
 		showFooter: function () {
 				// bind click
-				$('#footer').click(function () {
+				var footer = $('#footer');
+				$('#content').resize(function () {
+						var bodyH = $('body').height(), windowH = $(window).height();
+						if (bodyH < windowH) {
+								footer.css({'position': 'fixed', 'bottom': '0'});
+						} else {
+								footer.css({'position': 'relative', 'bottom': ''});
+						}
+				});
+
+
+				footer.click(function () {
 						$('.footer-detail').slideToggle('fast');
 				}).mouseleave(function () {
 						$('.footer-detail').slideUp('fast');
